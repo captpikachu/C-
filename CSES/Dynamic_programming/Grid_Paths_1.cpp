eab0abec4acb046d1b7e75384da2db6e4fc4a45b
captpikachu <capt.pikachu2005@gmail.com>
2025-12-25 11:27:07 +0530

    Update Grid_Paths_1.cpp

--- CSES/Dynamic_programming/Grid_Paths_1.cpp
+++ CSES/Dynamic_programming/Grid_Paths_1.cpp
@@ -1,11 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int MOD = 1e9+7;
+
 // dp[i][j] = dp[i-1][j] + dp[i][j-1]
 // if * dp[i][j]=0;
 int num_paths(vector<vector<char>> &grid,int i,int j,vector<vector<int>> & dp){
-    
+if (i < 0 || j < 0) return 0;
+if(dp[i][j]!=-1)
+return dp[i][j];
+if(grid[i][j]=='*')
+return dp[i][j]=0;
 
-
+if(i==0 && j==0)
+return dp[i][j]=1;  
+else if(i==0)
+return dp[i][j]=num_paths(grid,i,j-1,dp);
+else if(j==0)
+return dp[i][j]=num_paths(grid,i-1,j,dp);
+return dp[i][j]=(num_paths(grid,i-1,j,dp)+num_paths(grid,i,j-1,dp))%MOD;
 
 }
 
@@ -23,6 +35,8 @@ for(int i=0;i<n;i++){
     }
     grid.emplace_back(temp);
 }
+vector<vector<int>> dp(n,vector<int>(n,-1));
+cout<<num_paths(grid,n-1,n-1,dp);
 
 return 0;
 }
